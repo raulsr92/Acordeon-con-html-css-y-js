669fc8cdda1a964c35f58998688f9c9885904431
Raul Sanchez R. <raul.sr92@gmail.com>
2024-04-02 10:17:02 -0500

    Interacción JS N° 02 - Ocultar y mostrar panel

--- interaccion.js
+++ interaccion.js
@@ -3,21 +3,30 @@
 let menuOption, i;
 
 menuOption = document.getElementsByClassName("acordeon__opcion");
+console.log(menuOption);
 
-/*
-Inicializador: variable contadora de vueltas
-i=0 --> 1° vuelta  // i=1 --> 2° vuelta  // i=2 --> 3° vuelta
-
-Condición: que el ciclo dure 3 vueltas: menuOption.length = 3
-
-Actualizador del contador: i++ incremento
-
-*/
 
 for(  i=0; i<menuOption.length  ;  i++){
+
     menuOption[i].addEventListener("click", function () {
         
+        /* Efecto hover al hacer click */
         this.classList.toggle("active");     
+
+        /* Mostrar y Ocultar el panel debajo de cada opción */
+
+        var panel = this.nextElementSibling;
+        console.log(panel);
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else{
+            panel.style.display = "block"
+        }
+
+        
+
+
 
     })
 }
@@ -25,17 +34,7 @@ for(  i=0; i<menuOption.length  ;  i++){
 
 
 
+
 /*
-for (i = 0; i < menuOption.length; i++) {
 
-    console.log(`i=  ${i}`) 
-
-    menuOption[i].addEventListener("click", function() {
-        this.classList.toggle("active");     
-        console.log(`Al dar click, i = ${i}`) 
-    });
-
-    console.log(`i=  ${i}`) 
-}
-console.log(`i=  ${i}`) 
 */
